ac1227128ef24b4ef92834422f9522d6d523169b
hit9 <nz2324@126.com>
2013-03-24 15:43:16 +0800

    rm the g

--- golb/_.py
+++ golb/_.py
@@ -1,7 +1,12 @@
 #coding=utf8
 
 # import settings
-from .config import *
+from .config import src
+from .config import output
+from .config import templates
+from .config import name
+from .config import description
+from .config import author
 
 # force utf8!
 charset = "utf-8"
--- golb/g.py
+++
@@ -1,9 +0,0 @@
-#coding=utf8
-#
-# g object,  store settings from source markdown files
-#
-
-class g(object):
-
-    def __init__(self):
-        pass
