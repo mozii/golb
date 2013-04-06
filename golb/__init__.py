27139287f1716dc615328d8a2d64518973283cbf
hit9 <nz2324@126.com>
2013-04-06 11:42:03 +0800

    restructure the first time.And,TODO:gravatar_id and update template

--- golb/__init__.py
+++
@@ -1,3 +0,0 @@
-#
-# static blog generator for programmers
-#
--- golb/blog.py
+++ golb/blog.py
@@ -1,88 +1,92 @@
-# coding=utf8
+#coding=utf8
 from ._ import src as s
 from ._ import output as o
 from ._ import srcExt as se
 from ._ import outputExt as oe
-from .conf import conf
 from os.path import join as j
 from os.path import getmtime
 from datetime import datetime
 
 
-sel = len(se)
-
-
 class Post(object):
 
-    sdir = j(s, "post")  # source dir
-    odir = j(o, "post")  # output dir
-    tpl = "post.html"
+    src_dir = j(s, "post")  # source's dir
+    out_dir = j(o, "post")  # output's dir
+    template = "post.html"  # all posts are rendered with this template
 
-    def __init__(self, fn):  # init one Post object by filename
-        name = fn[:-sel]
-        self.name = name  # post's name i.e. "hellowrold"
-        self.filename = fn  # post's filename i.e. "hellowrold.md"
-        self.srcp = j(Post.sdir, name + se)  # source path
-        self.outp = j(Post.odir, name + oe)  # output path
-        self.update_at = datetime.fromtimestamp(getmtime(self.srcp))
+    def __init__(self, filename):
+        """
+        filename
+          post's filename with extension i.e. "I-am-a-post.md"
+        """
+        self.name = filename[:-len(se)]  # post's name i.e. "I-am-a-post"
+        self.filename = filename
+        self.src = j(Post.src_dir, self.name + se)  # source path
+        self.out = j(Post.out_dir, self.name + oe)  # output path
+        # post's update time
+        self.update_at = datetime.fromtimestamp(getmtime(self.src))
 
 
 class Tag(object):
 
-    odir = j(o, "tag")
-    tpl = "tag.html"
+    out_dir = j(o, "tag")
+    template = "tag.html"
 
     def __init__(self, name, posts=list()):
+        """
+        name
+          tag's name  i.e.  "I-am-a-tag"
+        posts
+          list of post taged this name
+        """
         self.name = name
         self.posts = posts
-        self.outp = j(Tag.odir, name + oe)
+        self.out = j(Tag.out_dir, name + oe)
 
 
 class Page(object):
 
-    odir = j(o, "page")
-    tpl = "page.html"
+    out_dir = j(o, "page")
+    template = "page.html"
 
     def __init__(self, number, posts=list()):
         self.number = number
         self.posts = posts
-        self.outp = j(Page.odir, str(number) + oe)
-        self.first = False
-        self.last = False
+        self.out = j(Page.out_dir, str(number) + oe)
+        self.first = False  # is the first page?
+        self.last = False  # is the last page?
 
 
 # other pages
 class Other(object):
 
     def __init__(self, **attrs):
-        for k, v in attrs.items():
-            setattr(self, k, v)
+        # set attributes to self
+        for name, value in attrs.items():
+            setattr(self, name, value)
 
-# index page
-index = Other(outp=j(o, "index" + oe))
 
+# index
+index = Other(out=j(o, "index" + oe))
 
 # archives page
 archives = Other(
-    outp=j(o, "archives" + oe),
-    tpl="archives.html"
+    out=j(o, "archives" + oe),
+    template="archives.html"
 )
 
-# about page (src:about.md, outp: about.html)
+# about.html
 about = Other(
-    srcp=j(s, "about" + se),
-    outp=j(o, "about" + oe),
-    tpl="about.html"
+    src=j(s, "about" + se),
+    out=j(o, "about" + oe),
+    template="about.html"
 )
 
-# tags page (outp: tags.html)
+# tags
 tags = Other(
-    outp=j(o, "tags" + oe),
-    tpl="tags.html"
+    out=j(o, "tags" + oe),
+    template="tags.html"
 )
 
-# feed.atom
-
-feed = Other(
-    outp=j(o, "feed.atom")
-)
+# feed
+feed = Other(out=j(o, "feed.atom"))
